2369816628bb9592934693791038f3143ef59e66
quqiufeng <quqiufeng@example.com>
2026-02-09 13:16:57 +0000

    句子区块增加间距：英文和中文之间加两个换行

--- translate.py
+++ translate.py
@@ -262,7 +262,7 @@ def create_trans_file(title, paragraphs, translations, vocab_list, vocab_trans, 
     content += "---\n\n"
     content += "SENTENCES:\n"
     for i in range(len(sent_list)):
-        content += f"{i+1}. {wrap_english(sent_list[i])}\n{wrap_chinese(sent_trans[i])}\n"
+        content += f"{i+1}. {wrap_english(sent_list[i])}\n\n{wrap_chinese(sent_trans[i])}\n\n"
 
     with open(output_path, 'w', encoding='utf-8') as f:
         f.write(content)
